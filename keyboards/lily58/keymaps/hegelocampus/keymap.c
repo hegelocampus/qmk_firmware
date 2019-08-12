2c34f7c9c28b0e392194b6baf4a0cbd92b6e859b
Bee <pizzaman8099@gmail.com>
2019-08-12 11:22:30 -0700

    Move del up to top right corner

--- keyboards/lily58/keymaps/hegelocampus/keymap.c
+++ keyboards/lily58/keymaps/hegelocampus/keymap.c
@@ -87,6 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
 <<<<<<< HEAD
+<<<<<<< HEAD
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_DEL, \
   _______, KC_F11,  KC_F12,  KC_LCBR, KC_RCBR, _______,                   _______, KC_UNDS, KC_PLUS, KC_PIPE, KC_COLON, _______,\
 =======
@@ -97,6 +98,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 =======
   _______, KC_F11,  KC_F12,  KC_LCBR, KC_RCBR, _______,                   _______, KC_UNDS, KC_PLUS, KC_PIPE, KC_COLON, KC_DEL,\
 >>>>>>> Adds small changes to lily58/keymaps/hegelocampus/keymap.c
+=======
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_DEL, \
+  _______, KC_F11,  KC_F12,  KC_LCBR, KC_RCBR, _______,                   _______, KC_UNDS, KC_PLUS, KC_PIPE, KC_COLON, _______,\
+>>>>>>> Move del up to top right corner
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LBRC, KC_RBRC,  _______,\
   _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, KC_TILD, KC_UNDS, _______, KC_LCBR, KC_RCBR,  KC_PIPE,\
                              _______, _______, _______, _______, _______, _______, _______, _______\
