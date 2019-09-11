b8d4aa486af69d546635b8e7015e16858a20500f
Bee <pizzaman8099@gmail.com>
2019-09-10 18:33:47 -0700

    Adjust tap_mod settings

--- keyboards/lily58/keymaps/hegelocampus/config.h
+++ keyboards/lily58/keymaps/hegelocampus/config.h
@@ -35,7 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define PERMISSIVE_HOLD
 //#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 80 
+#define TAPPING_TERM 250
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
