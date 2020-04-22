a0e0a14600b99d16091ffec5de027041d671acab
Bee <pizzaman8099@gmail.com>
2020-04-22 14:16:17 -0700

    Make minor tweaks to keymap

--- keyboards/lily58/keymaps/hegelocampus/keymap.c
+++ keyboards/lily58/keymaps/hegelocampus/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                     KC_F,    KC_G,     KC_C,    KC_R,    KC_L,    KC_BSPC,\
   KC_ESC,   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                     KC_D,    KC_H,     KC_T,    KC_N,    KC_S,    KC_ENT, \
   KC_LSPO,  KC_SLSH, KC_Q,    KC_J,    KC_K,    KC_X,  KC_LBRC,  KC_RBRC, KC_B,    KC_M,     KC_W,    KC_V,    KC_Z,    KC_RSPC,\
-                              KC_LALT, KC_LGUI, LOWER, KC_SPC,     RSPA,  RAISE,   KC_LCTRL, KC_LEAD \
+                              KC_LALT, KC_LGUI, LOWER, KC_SPC,     RSPA,  RAISE,   KC_LCTRL, KC_LALT \
 ),
 
 [_GAME] = LAYOUT( \
@@ -86,11 +86,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT( \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_DEL, \
-  _______, KC_F11,  KC_F12,  KC_LCBR, KC_RCBR, _______,                   _______, KC_UNDS, KC_PLUS, KC_PIPE, KC_COLON, _______,\
-  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LBRC, KC_RBRC,  _______,\
-  _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, KC_TILD, KC_UNDS, _______, KC_LCBR, KC_RCBR,  KC_PIPE,\
-                             _______, _______, _______, _______, _______, _______, _______, _______\
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_DEL, \
+  _______, KC_F11,  KC_F12,  KC_LCBR, KC_RCBR, _______,                   KC_COLON, KC_UNDS, KC_PLUS, KC_PIPE, _______, _______,\
+  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC,  KC_AMPR, KC_ASTR, KC_LBRC, KC_RBRC,  _______,\
+  _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, KC_TILD,  KC_UNDS, _______, KC_LCBR, KC_RCBR,  KC_PIPE,\
+                             _______, _______, _______, _______, _______, _______,  _______, _______\
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -109,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT( \
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______,  _______, _______, _______, \
-  KC_GRV,  _______, _______, KC_LBRC, KC_RBRC, _______,                     _______, KC_MINS,  KC_EQL , KC_BSLS, KC_SCLN, _______, \
+  KC_GRV,  _______, _______, KC_LBRC, KC_RBRC, _______,                     KC_SCLN, KC_MINS,  KC_EQL , KC_BSLS, _______, _______, \
   _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    _______, \
   _______, _______, _______, _______, _______, _______,  _______, _______,  KC_PGDN, KC_PGUP, KC_GRAVE, KC_LBRC, KC_RBRC, KC_BSLS, \
                              _______, _______, _______,  _______, _______,  _______, _______, _______ \
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_FN] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______,  _______, _______, \
-  _______, _______, _______, _______, _______, _______,                    _______, KC_TILD, KC_SLSH, KC_BSLS,  KC_SCLN, _______, \
+  _______, _______, _______, _______, _______, _______,                    KC_SCLN, KC_TILD, KC_SLSH, KC_BSLS,  _______, _______, \
   _______, _______, _______, _______, _______, _______,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, \
   _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______,  _______, _______, \
                              _______, _______, _______,  _______, _______, _______, _______, _______ \
@@ -189,8 +189,8 @@ void matrix_scan_user(void) {
 void matrix_render_user(struct CharacterMatrix *matrix) {
   if (is_master) {
     // If you want to change the display of OLED, you need to change here
-    matrix_write_ln(matrix, read_layer_state());
-    matrix_write_ln(matrix, read_keylog());
+    //matrix_write_ln(matrix, read_layer_state());
+    //matrix_write_ln(matrix, read_keylog());
     matrix_write_ln(matrix, read_keylogs());
     //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
     //matrix_write_ln(matrix, read_host_led_state());
