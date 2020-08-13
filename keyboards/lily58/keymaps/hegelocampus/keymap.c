00f9f958115186bb2d091882c7b07fbf9480f556
Bee <pizzaman8099@gmail.com>
2020-08-13 10:16:40 -0700

    Fix indentation and add caps lock to raise layer

--- keyboards/lily58/keymaps/hegelocampus/keymap.c
+++ keyboards/lily58/keymaps/hegelocampus/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 
- [_DVORAK] = LAYOUT( \
+[_DVORAK] = LAYOUT( \
   KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_BSLS,\
   KC_TAB,   KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                     KC_F,    KC_G,     KC_C,    KC_R,    KC_L,    KC_BSPC,\
   KC_ESC,   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                     KC_D,    KC_H,     KC_T,    KC_N,    KC_S,    KC_ENT, \
@@ -65,12 +65,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_GAME] = LAYOUT( \
- XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,     KC_5,                     KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_BSLS,\
- KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                     KC_F,    KC_G,     KC_C,    KC_R,    KC_L,    KC_BSPC,\
- XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,                     KC_H,    KC_J,     KC_K,    KC_L,    KC_QUOT, KC_ENT, \
- KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,  KC_LCTRL,  KC_ESC, KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,\
+  XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,     KC_5,                     KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_BSLS,\
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                     KC_F,    KC_G,     KC_C,    KC_R,    KC_L,    KC_BSPC,\
+  XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,                     KC_H,    KC_J,     KC_K,    KC_L,    KC_QUOT, KC_ENT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,  KC_LCTRL,  KC_ESC, KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,\
                             KC_LALT, KC_LCTRL, LOWER, KC_SPC,    RSPA,   RAISE,   KC_LCTRL, KC_LEAD \
 ),
+
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
@@ -92,6 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, KC_TILD,  KC_UNDS, _______, KC_LCBR, KC_RCBR,  KC_PIPE,\
                              _______, _______, _______, _______, _______, _______,  _______, _______\
 ),
+
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
@@ -110,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______,  _______, _______, _______, \
   KC_GRV,  _______, _______, KC_LBRC, KC_RBRC, _______,                     KC_SCLN, KC_MINS,  KC_EQL , KC_BSLS, _______, _______, \
-  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    _______, \
+  KC_CAPS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    _______, \
   _______, _______, _______, _______, _______, _______,  _______, _______,  KC_PGDN, KC_PGUP, KC_GRAVE, KC_LBRC, KC_RBRC, KC_BSLS, \
                              _______, _______, _______,  _______, _______,  _______, _______, _______ \
 ),
@@ -213,6 +215,7 @@ void iota_gfx_task_user(void) {
   matrix_render_user(&matrix);
   matrix_update(&display, &matrix);
 }
+
 #endif//SSD1306OLED
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -277,3 +280,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
+
